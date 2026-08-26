b759c3ccbc9fb0f11fcbd9476b4aa8bc36d4af33
Alejandro Nebot <neflodeveloper@gmail.com>
2026-08-27 01:03:59 +0200

    build: implement exception handling

---
+++ src/main/java/neflo/dev/config/CustomExceptionHandler.java
@@ -0,0 +1,94 @@
+package neflo.dev.config;
+
+import lombok.extern.slf4j.Slf4j;
+import neflo.dev.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(value = {NoEntitiesFoundException.class})
+    public ResponseEntity<CustomErrorResponse> handleNoEntitiesFound(NoEntitiesFoundException exception) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomErrorResponse response = new CustomErrorResponse(
+                HttpStatus.NOT_FOUND,
+                exception.getErrorCode(),
+                exception.getMessage()
+        );
+        log.error("TripCount.ExceptionHandler >> NO ENTITIES FOUND EXCEPTION {} :: {}", exception.getErrorCode(), exception.getMessage());
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(value = {ValidationException.class})
+    public ResponseEntity<CustomErrorResponse> handleValidationException(ValidationException exception) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomErrorResponse response = new CustomErrorResponse(
+                HttpStatus.NOT_FOUND,
+                exception.getErrorCode(),
+                exception.getMessage()
+        );
+        log.error("TripCount.ExceptionHandler >> VALIDATION EXCEPTION {} :: {}", exception.getErrorCode(), exception.getMessage());
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(value = {DatabaseException.class})
+    public ResponseEntity<CustomErrorResponse> handleDatabaseException(DatabaseException exception) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        CustomErrorResponse response = new CustomErrorResponse(
+                HttpStatus.NOT_FOUND,
+                exception.getErrorCode(),
+                exception.getMessage()
+        );
+        log.error("TripCount.ExceptionHandler >> DATABASE EXCEPTION {} :: {}", exception.getErrorCode(), exception.getMessage());
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(value = {UnexpectedException.class})
+    public ResponseEntity<CustomErrorResponse> handleUnexpectedException(UnexpectedException exception) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        CustomErrorResponse response = new CustomErrorResponse(
+                HttpStatus.NOT_FOUND,
+                exception.getErrorCode(),
+                exception.getMessage()
+        );
+        log.error("TripCount.ExceptionHandler >> UNEXPECTED EXCEPTION {} :: {}", exception.getErrorCode(), exception.getMessage());
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(value = {AuthenticationException.class})
+    public ResponseEntity<CustomErrorResponse> handleAuthenticationException(AuthenticationException exception) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        CustomErrorResponse response = new CustomErrorResponse(
+                HttpStatus.NOT_FOUND,
+                exception.getErrorCode(),
+                exception.getMessage()
+        );
+        log.error("TripCount.ExceptionHandler >> AUTHENTICATION EXCEPTION {} :: {}", exception.getErrorCode(), exception.getMessage());
+
+        return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<CustomErrorResponse> handleGenericException(Exception exception) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        String errorCode = "generic-exception";
+        String message = "An unexpected error occurred, we are working on it.";
+        CustomErrorResponse response = new CustomErrorResponse(
+                HttpStatus.NOT_FOUND,
+                errorCode,
+                message
+        );
+        log.error("TripCount.ExceptionHandler >> GENERIC EXCEPTION {} :: {}", errorCode, message);
+
+        return new ResponseEntity<>(response, status);
+    }
+
+}
